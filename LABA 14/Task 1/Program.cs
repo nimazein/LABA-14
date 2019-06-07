1c152bb8dd23ab86a8ec81003f29a402bca98d49
nimazein <nimazein@edu.hse.ru>
2019-06-07 13:47:42 +0500

    Full LABA

--- LABA 14/Task 1/Program.cs
+++ LABA 14/Task 1/Program.cs
@@ -17,33 +17,11 @@ namespace Task_1
             Console.ReadKey();
             Console.Clear();
 
+            Console.WriteLine("C использованием LINQ запросов");
+            UsingLINQ();
 
-            Console.WriteLine("Запрос 1: все птицы в зоопарке");
-            Console.WriteLine();
-            ShowBirds_1();
-            Console.ReadKey();
-            Console.Clear();
-
-
-            Console.WriteLine("Запрос 2: количество млекопитающих в зоопарке");
-            Console.WriteLine();
-            CountMammals_2();
-            Console.ReadKey();
-            Console.Clear();
-
-
-            Console.WriteLine("Запрос 3: операции над множествами (секция 1 и 2)");
-            Console.WriteLine();
-            SetOperations_3();
-            Console.ReadKey();
-            Console.Clear();
-
-            Console.WriteLine("Запрос 4: агрегирование данных");
-            Console.WriteLine();
-            AgregateData_4();
-            Console.ReadKey();
-            Console.Clear();
-            
+            Console.WriteLine("C использованием методов расширения");
+            UsingDelegates();
         }
         private static void FillCollections()
         {
@@ -104,6 +82,37 @@ namespace Task_1
             Console.Write("Введите размер: ");
             Size = Convert.ToInt32(Console.ReadLine());
         }
+
+
+        // Using LINQ
+        private static void UsingLINQ()
+        {
+            Console.WriteLine("Запрос 1: все птицы в зоопарке");
+            Console.WriteLine();
+            ShowBirds_1();
+            Console.ReadKey();
+            Console.Clear();
+
+
+            Console.WriteLine("Запрос 2: количество млекопитающих в зоопарке");
+            Console.WriteLine();
+            CountMammals_2();
+            Console.ReadKey();
+            Console.Clear();
+
+
+            Console.WriteLine("Запрос 3: операции над множествами (секция 1 и 2)");
+            Console.WriteLine();
+            SetOperations_3();
+            Console.ReadKey();
+            Console.Clear();
+
+            Console.WriteLine("Запрос 4: агрегирование данных");
+            Console.WriteLine();
+            AgregateData_4();
+            Console.ReadKey();
+            Console.Clear();
+        }
         private static void ShowBirds_1()
         {
             int sectionNumber = 1;
@@ -114,6 +123,7 @@ namespace Task_1
                 sectionNumber++;
             }
         }
+ 
         private static void CountMammals_2()
         {
             int inSection = 0;
@@ -193,6 +203,137 @@ namespace Task_1
 
             }
             Console.WriteLine($"Средний вес млекопитающих в зоопарке: {weightSumInZoo / numberInZoo}");
+        }
+
+        // Using Delegates
+        private static void UsingDelegates()
+        {
+            Console.WriteLine("Запрос 1: все птицы в зоопарке");
+            Console.WriteLine();
+            ShowBirds_1_UsingDelegates();
+            Console.ReadKey();
+            Console.Clear();
+
+            Console.WriteLine("Запрос 2: количество млекопитающих в зоопарке");
+            Console.WriteLine();
+            CountMammals_2_UsingDelegates();
+            Console.ReadKey();
+            Console.Clear();
+
+            Console.WriteLine("Запрос 3: операции над множествами (секция 1 и 2)");
+            Console.WriteLine();
+            SetOperations_3_UsingDelegates();
+            Console.ReadKey();
+            Console.Clear();
+
+            Console.WriteLine("Запрос 4: агрегирование данных");
+            Console.WriteLine();
+            AgregateData_4_UsingDelegates();
+            Console.ReadKey();
+            Console.Clear();
+
+        }
+        private static void ShowBirds_1_UsingDelegates()
+        {
+            int sectionNumber = 1;
+            foreach (Sections section in zoo.Zooo)
+            {
+                Func<IAnimal, bool> searchFilter = delegate (IAnimal bird) { return bird.Name.Contains("Птица"); };
+                Func<IAnimal, IAnimal> itemToProcess = delegate (IAnimal bird) { return bird; };
+
+                var birds = section.Animals.Where(searchFilter).Select(itemToProcess);
+
+                foreach (var s in birds) Console.WriteLine("Птица: {0} в секции {1}", s.ToString(), sectionNumber);
+                sectionNumber++;
+            }  
+        }
+        private static void CountMammals_2_UsingDelegates()
+        {
+            int inSection = 0;
+            int sectionNumber = 1;
+            int inZoo = 0;
+            foreach (Sections section in zoo.Zooo)
+            {
+                Func<IAnimal, bool> searchFilter = delegate (IAnimal mammal) { return mammal.Name.Contains("Млекопитающее"); };
+                Func<IAnimal, IAnimal> itemToProcess = delegate (IAnimal mammal) { return mammal; };
+
+                inSection = section.Animals.Where(searchFilter).Select(itemToProcess).Count<IAnimal>();
+                Console.WriteLine($"В секции {sectionNumber} млекопитающих: {inSection}");
+
+                inZoo += inSection;
+                sectionNumber++;
+            }
+            
+            Console.WriteLine($"Млекопитающих в зоопарке: {inZoo}");
+        }
+        private static void SetOperations_3_UsingDelegates()
+        {
+            IAnimal[] collection_1 = zoo.Zooo[0].ToArray();
+            IAnimal[] collection_2 = zoo.Zooo[1].ToArray();
+
+            Console.WriteLine("Пересечение множеств");
+            Console.WriteLine();
+            Intersect_UsingDelegates(collection_1, collection_2);
+            Console.ReadKey();
+            Console.Clear();
+
+            Console.WriteLine("Объединение множеств");
+            Console.WriteLine();
+            Union_UsingDelegates(collection_1, collection_2);
+            Console.ReadKey();
+            Console.Clear();
+
+            Console.WriteLine("Разность множеств");
+            Console.WriteLine();
+            Except_UsingDelegates(collection_1, collection_2);
+            Console.ReadKey();
+            Console.Clear();
+        }
+        private static void Intersect_UsingDelegates(IAnimal[] collection_1, IAnimal[] collection_2)
+        {
+            IEnumerable<IAnimal> Intersecttt(IAnimal[] collection) => collection_1.Intersect(collection);
+
+            var intersection = Intersecttt(collection_2);
+            foreach (IAnimal animal in intersection)
+            {
+                Console.WriteLine(animal.ToString());
+            }
+
+        }
+        private static void Union_UsingDelegates(IAnimal[] collection_1, IAnimal[] collection_2)
+        {
+            IEnumerable<IAnimal> Unionnn(IAnimal[] collection) => collection_1.Union(collection);
+
+            var union = Unionnn(collection_2);
+
+            foreach (IAnimal animal in union)
+            {
+                Console.WriteLine(animal.ToString());
+            }
+        }
+        private static void Except_UsingDelegates(IAnimal[] collection_1, IAnimal[] collection_2)
+        {
+            IEnumerable<IAnimal> Excepttt(IAnimal[] collection) => collection_1.Except(collection);
+
+            var except = Excepttt(collection_2);
+            foreach (IAnimal animal in except)
+            {
+                Console.WriteLine(animal.ToString());
+            }
+        }
+        private static void AgregateData_4_UsingDelegates()
+        {
+            int averageSum = 0;
+            int sectionNumber = 1;
+            foreach (Sections section in zoo.Zooo)
+            {             
+                var averageInSection = section.Animals.Where(mam => mam.Name.Contains("Млекопитающее")).Select(mam => mam.Weight).Average();
+
+                Console.WriteLine($"Средний вес млекопитающих в секции {sectionNumber}: {Convert.ToInt32(averageInSection)}");
+                averageSum += Convert.ToInt32(averageInSection);
+                sectionNumber++;
+            }
+            Console.WriteLine($"Средний вес млекопитающих в зоопарке: {averageSum/zoo.Zooo.Count}");
         }
     }
 }
